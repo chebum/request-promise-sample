1acf8166aae60f54a599e37102557a6f7c93a9d9
Ivan Nikitin <cherniy.bumer@gmail.com>
2020-03-21 08:43:41 +0100

    modifies the project for browser

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
 import * as Bluebird from "bluebird";
-import * as rp from "request-promise";
 
 // Bluebird is assignable to global.Promise if @types/node@12 is being used.
 // See: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/42084
-global.Promise = Bluebird;
+window.Promise = Bluebird;
 
-// "request-promise" returns a `RequestPromise` which is not a regular "native" `Promise`.
-const promise: rp.RequestPromise = rp("https://www.google.com");
-
-// `rp(...).promise()` returns the `Bluebird` promise according to the "request-promise" documentation. (Please read it!)
-const bluebird: Bluebird<any> = rp("https://www.google.com").promise();
-
-// You can easily assign a `Bluebird` promise to the native `Promise` interface.
-const native: Promise<any> = bluebird;
+async function testFn(): Bluebird<void> {
+	return Bluebird.delay(100);
+}
